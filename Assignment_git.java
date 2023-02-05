d1f2bba7561ca63162b2823d2121e26a6c374364
priyavats407 <109791209+priyavats407@users.noreply.github.com>
2023-02-05 23:15:08 +0530

    Add files via upload

---
+++ Assignment_git.java
@@ -0,0 +1,53 @@
+Que 1:How to check if git is available on your system?
+
+Ans:- Open Command prompt and type git--version.
+
+
+=========================================================
+
+Que2:How to initialize a new git repository?
+
+Ans:  git -init
+
+=========================================================
+
+     Que3:How to tell git about your name and email?
+ Ans:
+
+For name:  git config --global user.name “<your_name>”
+
+For email:  git config --global user.email “<email>”
+
+=========================================================
+ 
+Que4: How to add a file to the staging area?
+
+Ans: git --add.
+
+=========================================================
+
+Que5:How to remove a file from the staging area?
+
+Ans:git reset fileName
+
+=========================================================
+
+Que6: How to make a commit?
+
+Ans: git commit -m ‘<message>’ 
+
+=========================================================
+
+Que7: How to send your changes to a remote repository?
+
+Ans:  git push -u origin main.
+=========================================================
+Que8: What is difference between clone and pull?
+
+Ans:
+
+Clone:-- is used to clone the repository from gitHub to the 
+User’s device/system.
+Pull:-- to update that local file with new commits from the remote repository.
+
+
